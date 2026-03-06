8f17a04de679390dabb157af953096f21154d3af
PipperjoaopauloINFO5 <pipperjoaopaulo@gmail.com>
2026-03-06 08:42:36 -0300

    Adicionando metodos do ciclo de vida do app

--- app/src/main/java/com/example/projetoemulador/MainActivity.kt
+++ app/src/main/java/com/example/projetoemulador/MainActivity.kt
@@ -1,20 +1,45 @@
 package com.example.projetoemulador
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import kotlin.math.log
 
 class MainActivity : AppCompatActivity() {
+    private val TAG ="INFOTESTE"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        Log.i(TAG, "OnCreate")
+
         }
+    /*2 esta visivel ao usuario*/
+    override fun onStart() {
+        super.onStart()
+        Log.i(TAG, "OnStart")
     }
-}
+    /*3 pronta para usar*/
+    override fun onResume() {
+        super.onResume()
+        Log.i(TAG, "OnResume")
+    }
+    /*4 saindo da activity*/
+    override fun onPause() {
+        super.onPause()
+        Log.i(TAG, "OnPause")
+    }
+    /*5 esta invisivel ao usuario*/
+    override fun onStop() {
+        super.onStop()
+        Log.i(TAG, "OnStop")
+    }
+    /*6 remove activity da memoria*/
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.i(TAG, "OnDestroy")
+    }
+    }
